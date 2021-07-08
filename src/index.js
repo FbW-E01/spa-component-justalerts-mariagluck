58abd4a1a7d8186809becd3fa2111d26de5e5f82
mariagluck <mariamart.berlin@gmail.com>
2021-07-08 14:45:21 +0200

    done!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Box = (/* ???? */) => {
-
-  // ?????
+const Box = (props) => {
+  
+     const color = (color) => {
+        switch (color) {
+            case 'blue': return 'alert-primary'; 
+            case 'red': return 'alert-danger'; 
+            case 'orange': return 'alert-warning'; 
+            default: return 'alert-dark';
+         }
+       }
+   
+     if (!props.hide) console.log(props);
+   
+     return(
+         !props.hide ?
+             <div className={`alert ${color(props.type)}`} role="alert">
+                 <h4 className="alert-heading">ALERT! Alert!</h4>
+                 <p>Aww yeah, you successfully read this important alert message.</p>
+                 <hr></hr>
+                 <p className="mb-0">Beautiful alert.</p>
+             </div>
+          : "" 
+     )
   
 };
 
@@ -17,3 +37,5 @@ const jsx = <>
 const element = document.getElementById("target");
 
 ReactDOM.render(jsx, element);
+
+
